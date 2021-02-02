742aef66d01cab6ca4290afa2d9442804933c673
nevernkz <>
2021-02-03 00:05:09 +0700

    UpdateNodeJs

--- Week2/NodeJs/Assets/Script/WebsocketConnection.cs
+++ Week2/NodeJs/Assets/Script/WebsocketConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using WebSocketSharp;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace ProgramChat
 {
@@ -12,23 +13,63 @@ namespace ProgramChat
         private WebSocket websocket;
         public InputField inputIP;
         public InputField inputPort;
-        public string inputip, inputport;
-
+        public string inputip, inputport,chatstr = null;
+        private SceneManager sceneIP;
+        public InputField Chat;
+        public GameObject ChatUI;
+        public GameObject ChatTextShow;
+        public GameObject ChatTextshows;
+        public string ChatTextshow = null;
+        public string[] ListChat;
+        int i = 0;
         
-        /*void Start()
+        
+        
+        private void Awake()
         {
-            websocket = new WebSocket("ws://127.0.0.1:25500/");
+            ChatUI.SetActive(false);
+            ChatTextShow.SetActive(false);
+            
+            
+            
+        }
+
+        /*
+        void Start()
+        {
+            
+              websocket = new WebSocket("ws://127.0.0.1:25500/");
 
             websocket.OnMessage += OnMessage;
 
             websocket.Connect();
+        }
+            */
 
-        }*/
+    
         void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Return))
+            chatstr = Chat.text;
+            if (Input.GetKeyDown(KeyCode.Return))
             {
-                websocket.Send("Number : " + Random.Range(0, 9999));
+                i++;
+                ListChat = new string[i];
+                
+
+               
+                Chat.text = "";
+                ChatTextshows.GetComponent<Text>().text = ChatTextshow;
+                
+                
+                //chatstr = Chat.text;
+                websocket.Send(chatstr);
+                
+
+                //if enter clear inputfield
+
+
+                //websocket.Send("Number : " + Random.Range(0, 9999));
+                
                 
 
             }
@@ -42,18 +83,26 @@ namespace ProgramChat
         }
         public void OnMessage(object sender, MessageEventArgs messageEventArgs)
         {
+            
             Debug.Log("Message form Server : " + messageEventArgs.Data);
+            ChatTextshow = messageEventArgs.Data;
+
+            ChatTextshows.GetComponent<Text>().text = ChatTextshow;
+            
+
         }
 
         public void ClickButtonToConnect()
         {
+            ChatUI.SetActive(true);
+            ChatTextShow.SetActive(true);
             inputip = inputIP.text;
             inputport = inputPort.text;
-
 
             websocket = new WebSocket("ws://"+inputip+":"+inputport+"/");
             websocket.OnMessage += OnMessage;
             websocket.Connect();
+            
         }
     }
 }
