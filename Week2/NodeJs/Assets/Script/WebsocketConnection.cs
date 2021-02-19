1879b7b546d7836a11c1e81a69cb632106f46293
DESKTOP-K890P5J\xAnnieNC <Kaittipong.jitk@bumail.net>
2021-02-20 06:00:18 +0700

    Send Assignment Week4

--- Week2/NodeJs/Assets/Script/WebsocketConnection.cs
+++ Week2/NodeJs/Assets/Script/WebsocketConnection.cs
@@ -9,162 +9,297 @@ using TMPro;
 
 namespace ProgramChat
 {
-
-    public class Messagedata
-    {
-        public string Username;
-        public string Message;
-
-        public Messagedata(string username, string message)
-        {
-            Username = username;
-            Message = message;
-        }
-    }
     public class WebsocketConnection : MonoBehaviour
     {
-        [SerializeField] Text text;
-        private WebSocket websocket;
+        public struct SocketEvent
+        {
+            public string eventName;
+            public string data;
+
+            public SocketEvent(string eventName, string data)
+            {
+                this.eventName = eventName;
+                this.data = data;
+            }
+        }
+        private WebSocket ws;
 
         public InputField Chat;
         public InputField UsernameText;
+        public InputField NameRoom;
+        public InputField InputNameRoom;
+        public InputField JoinName;
 
 
         public string username;
         public string message;
         public GameObject connectbutton;
         public GameObject ChatUI;
-        public GameObject ChatTextShowGameObject;
-        public GameObject ChatTextshows;
+
         public GameObject Scrollview;
         public GameObject ButtonSendMessage;
         public GameObject Chatpanel;
-        public GameObject TextGameObject;
-        public GameObject ChatShowText;
         public GameObject UsernameGameobject;
+        public GameObject CreateRooms;
+        public GameObject SelectJoinRoom,JoinRoomInput,JoinRoomGameOb;
+        public GameObject LeaveButton;
+        public GameObject PopupError,PopupErrorCreate;
+        public GameObject RoomNameTextGameob;
+        public GameObject CreateRoomGameOb,CreateRoomInputOb;
 
-        public GameObject TextMeshPro;
-
-
-
-
-        MessageEventArgs messageEventArgs;
-
-        private string inputip, inputport, chatstr = null;
         public string ChatTextshow;
         public string Textgameobjectother;
-
-        public string UserNumber;
-        public TextMeshProUGUI textMesh;
+        public string tempMessageString;
 
 
 
+        public Text SendChatText;
+        public Text ReceiveText;
+        public Text RoomNameText;
 
+
+
+        public delegate void DelegateHandle(SocketEvent result);
+        public DelegateHandle OnCreateRoom;
+        public DelegateHandle OnJoinRoom;
+        public DelegateHandle OnLeaveRoom;
+
+
+       
         private void Awake()
         {
             ChatUI.SetActive(false);
-            ChatTextShowGameObject.SetActive(false);
+            CreateRooms.SetActive(false);
+            SelectJoinRoom.SetActive(false);
             Scrollview.SetActive(false);
             ButtonSendMessage.SetActive(false);
-            ChatShowText.SetActive(false);
             UsernameGameobject.SetActive(true);
-            TextMeshPro.SetActive(false);
-
+            LeaveButton.SetActive(false);
         }
-        private void Start()
-        {
-
-        }
-
-
         void Update()
         {
-            chatstr = Chat.text;
+
             Textgameobjectother = Chat.text;
-            ChatTextshows.GetComponent<Text>().text = ChatTextshow;
-
-           
-
+            UpdateNotifyMessage();
         }
-        private void OnDestroy()
+        public void Start()
         {
-            if (websocket != null)
-            {
-                websocket.Close();
-            }
+            OnCreateRoom += CreateRoomDelegate;
+          
         }
         public void SendChat()
         {
-            var message = new Messagedata(username, Chat.text);
+
             var json = JsonUtility.ToJson(message);
-            websocket.Send(json);
+            ws.Send(json);
             Chat.text = null;
-            websocket.Send(Textgameobjectother);
+            ws.Send(Textgameobjectother);
         }
-        public void SendMessagetochat(string text)
+
+        public void SendMessagetochat()
         {
 
             SendChat();
         }
 
-
-        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
-        {
-            var data = JsonUtility.FromJson<Messagedata>(messageEventArgs.Data);
-            if (data.Username == username)
-            {
-                Debug.Log("Same Account ");
-                textMesh.text += "\n<align=right>" + data.Username + ": <align=right>" + data.Message;
-            }
-            else
-            {
-                Debug.Log("None Same Account ");
-                textMesh.text += "\n<align=left>" + data.Username + ": <align=left>" + data.Message;
-            }
-
-            Debug.Log("Message form Server : " + data.Message);
-            /*ChatTextshow += "\n" + messageEventArgs.Data;
-            //ChatTextshow += "\n" + messageEventArgs.Data;
-            textMesh.GetComponent<TextMeshProUGUI>().text = "\n<align=right>" + ChatTextshow;
-            textMesh.GetComponent<TextMeshProUGUI>().text = "\n<align=left>" + ChatTextshow;
-            ChatTextshows.GetComponent<Text>().text = ChatTextshow; */
-
-
-        }
-
         public void ClickButtonToConnect()
         {
 
-            //TextFormat
+            string url = "ws://127.0.0.1:25500/";
 
-
-            websocket = new WebSocket("ws://127.0.0.1:25500/");
+            ws = new WebSocket(url);
             username = UsernameText.text;
-            websocket.OnMessage += OnMessage;
-            websocket.Connect();
-            SetacttiveUI();
+            ws.OnMessage += OnMessage;
 
+            ws.Connect();
+            RoomUI();
         }
         public void SetacttiveUI()
         {
             //SetActive
             ChatUI.SetActive(true);
-            ChatTextShowGameObject.SetActive(false);
-
-            connectbutton.SetActive(false);
             Scrollview.SetActive(true);
             ButtonSendMessage.SetActive(true);
-            ChatShowText.SetActive(false);
-            TextMeshPro.SetActive(true);
+            CreateRooms.SetActive(false);
+            SelectJoinRoom.SetActive(false);
+        }
+       
+        public void LeaveRoom()
+        {
+            SocketEvent socketEvent = new SocketEvent("LeaveRoom", "");
 
-            UsernameGameobject.SetActive(false);
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            ws.Send(toJsonStr);
         }
 
+        public void RoomUI()
+        {
+            CreateRooms.SetActive(true);
+            SelectJoinRoom.SetActive(true);
+            connectbutton.SetActive(false);
+            UsernameGameobject.SetActive(false);
+        }
+        public void JoinintoChat()
+        {
+            SetacttiveUI();
+        }
+        public void Disconnect()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+        private void UpdateNotifyMessage()
+        {
+            if (string.IsNullOrEmpty(tempMessageString) == false)
+            {
+                SocketEvent receiveMessageData = JsonUtility.FromJson<SocketEvent>(tempMessageString);
+
+                if (receiveMessageData.eventName == "CreateRoom")
+                {
+                    if (OnCreateRoom != null)
+                    
+                        OnCreateRoom(receiveMessageData);
+                    Debug.Log(tempMessageString);
+                    //if(tempMessageString == "fail")
+                    //{
+                    //    PopupError.SetActive(true);
+                    //    LeaveButton.SetActive(true);
+                    //    RoomNameTextGameob.SetActive(false);
+                    //}
+                    //else
+                    //{
+                    //    JoinintoChat();
+                    //}
+
+                }
+                else if (receiveMessageData.eventName == "JoinRoom")
+                {
+                    if (OnJoinRoom != null)
+                    {
+                        OnJoinRoom(receiveMessageData);
+                    }
+                       
+
+                    if (JoinName.text == receiveMessageData.data)
+                    {
+                        JoinintoChat();
+                        LeaveButton.SetActive(true);
+                        RoomNameTextGameob.SetActive(true);
+                    }
+                    else
+                    {
+                        RoomNameTextGameob.SetActive(false);
+                        PopupError.SetActive(true);
+                        LeaveButton.SetActive(true);
+                    }
+
+                }
+                else if (receiveMessageData.eventName == "LeaveRoom")
+                {
+                    if (OnLeaveRoom != null)
+                        OnLeaveRoom(receiveMessageData);
+                }
+
+                tempMessageString = "";
+            }
+
+        }
+        private void OnDestroy()
+        {
+            if (ws != null)
+            {
+                ws.Close();
+            }
+        }
+
+       
+        public void CreateRoomButton()
+        {
+
+            SocketEvent socketEvent = new SocketEvent("CreateRoom", NameRoom.text);
+            //NameRoom.text = GetRoomText;
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            ws.Send(toJsonStr);
+            RoomNameText.text = NameRoom.text;
+            
+            Debug.Log(RoomNameText.text);
+            Debug.Log(NameRoom.text);
+            
+
+        }
+
+        public void JoinintoRoom()
+        {
+
+            SocketEvent socketEvent = new SocketEvent("JoinRoom", JoinName.text);
+
+            string toJsonStr = JsonUtility.ToJson(socketEvent);
+
+            //ws.Send(toJsonStr);
+            ws.Send(toJsonStr);
+            RoomNameText.text = JoinName.text;
+
+           
+
+
+        }
+        public void LeaveToActive()
+        {
+
+            RoomNameText.text = "";
+            PopupError.SetActive(false);
+            ChatUI.SetActive(false);
+            CreateRooms.SetActive(true);
+            SelectJoinRoom.SetActive(true);
+            Scrollview.SetActive(false);
+            ButtonSendMessage.SetActive(false);
+            UsernameGameobject.SetActive(false);
+            PopupErrorCreate.SetActive(false);
+        }
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+
+            tempMessageString = messageEventArgs.Data;
+            //Debug.Log("Message form Server : " + data.Message);
+            Debug.Log(messageEventArgs.Data);
+            if(messageEventArgs.Data== "false")
+            {
+                PopupErrorCreate.SetActive(true);
+                LeaveButton.SetActive(true);
+                RoomNameTextGameob.SetActive(false);
+
+            }
+
+
+
+
+        }
+        public void CreateRoomDelegate(SocketEvent state)
+        {
+            Debug.Log(state.data);
+
+            if (state.data == "fail")
+            {
+                PopupErrorCreate.SetActive(true);
+                LeaveButton.SetActive(true);
+                RoomNameTextGameob.SetActive(false);
+                CreateRoomGameOb.SetActive(false);
+                CreateRoomInputOb.SetActive(false);
+
+            }
+            else
+            {
+                JoinintoChat();
+                CreateRoomGameOb.SetActive(false);
+                CreateRoomInputOb.SetActive(false);
+                Debug.Log(state);
+            }
+        }
+
+      
     }
-   
+
 }
-    
-
-
-
 
